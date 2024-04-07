a948ae938b3db8f14cb6ce86fc5a1e650a8a4151
Son Do <dhson8396@gmail.com>
2024-04-07 16:40:08 +0800

    chore: adjust local ip alias

--- shell/aliases.zsh
+++ shell/aliases.zsh
@@ -47,7 +47,7 @@ alias library="cd $HOME/Library"
 
 # IP addresses
 alias ip="curl ipconfig.pw/json"
-alias localip="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
+alias localip="ifconfig -a | grep -o 'inet\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet? (addr:)? ?/, \"\"); print }'"
 
 # Laravel
 # alias a="php artisan"
